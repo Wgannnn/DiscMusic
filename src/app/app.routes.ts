d6d56d12e57a6013eadb37400992338b1411a096
Wgannnn <wiktor.wagan@gmail.com>
2026-03-04 13:20:19 +0100

    Rzeczy po zalogowaniu sie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Home } from './home/home';
 import { Playlists } from './playlists/playlists';
 import { Login } from './login/login';
 import { AuthGuard } from './auth.guard'; // <-- importujemy guard
+import { Profile } from './profile/profile';
+import { Settings } from './settings/settings';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'playlists', component: Playlists, canActivate: [AuthGuard] }, // <-- dodajemy guard
     { path: 'login', component: Login },
+    { path: 'profile', component: Profile, canActivate: [AuthGuard] },
+    { path: 'settings', component: Settings, canActivate: [AuthGuard] },
     { path: '**', redirectTo: '' }
 ];
